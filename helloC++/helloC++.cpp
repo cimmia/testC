6fa39a6e201746ef0dd5ba4e8e1069fc0fe3f301
cimmia <cimmia@outlook.com>
2014-10-28 20:48:42 +0800

    这是第二次提交用来测试Git

---
+++ helloC++/helloC++.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+	double input = 0;
+	cout << "Hello,C++!"<<endl<<"Please input an integer:" << endl;
+	cin >> input;
+	cout << "What you have input is "<<input<<endl;
+	cout << "This sentence is to test Git";
+	system("pause");
+}
